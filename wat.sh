f558670948b84174b41eda6f39ed76e8e722862b
Damien <damien@it-consultis.com>
2016-08-04 14:18:46 +0800

    changed url

--- wat.sh
+++ wat.sh
@@ -7,7 +7,7 @@ NODE_VERSION=v6.3.1
 
 mkdir -p ~/.cache
 cd ~/.cache
-curl https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-linux-x64.tar.xz/ > node.tar.xz
+curl https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-linux-x64.tar.xz > node.tar.xz
 tar -xvf node.tar.xz
 cp node-$NODE_VERSION-linux-x64/bin/node .
 mv node bin
